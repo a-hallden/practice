3add1919ce6bf1a5975a0eeffecf413a91ce8b11
Andreas Hallden <p.andreas.hallden@gmail.com>
2020-12-07 19:22:48 +0100

    Add basic search view

--- api/views.py
+++ api/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+
+from .client import Client
+
 
 # Create your views here.
+class SearchView(generics.ListAPIView):
+    client = Client()
+
+    def get(self, request, *args, **kwargs):
+        search_term = request.GET.get('search', None)
+        if not search_term:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        response = self.client.search(search_term)
+        return Response(response.json(), status=status.HTTP_200_OK)
