f26f4112e5477031dda93c3ed2956cb67f84f905
Andreas Hallden <p.andreas.hallden@gmail.com>
2020-12-07 19:42:41 +0100

    Create Movie+Favorite models + migration

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from common.abstracts import TimeStampedModel
+
+
+class Movie(models.Model):
+    name = models.CharField(max_length=255)
+    imdbID = models.CharField(max_length=10)
+
+
+class Favorites(TimeStampedModel):
+    name = models.CharField(max_length=255)
+    movies = models.ManyToManyField(Movie, related_name='favorite_lists')
