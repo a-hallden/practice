a68dadf10454f464d995b0a1ad1443fc3c2e893e
Andreas Hallden <p.andreas.hallden@gmail.com>
2020-12-07 19:19:26 +0100

    Add basic client

---
+++ api/client.py
@@ -0,0 +1,31 @@
+import requests
+from django.conf import settings
+
+
+class APIResponse(object):
+    def __init__(self, data, success, status_code):
+        super(APIResponse, self).__init__()
+        self.data = data
+        self.success = success
+        self.status_code = status_code
+
+    def __repr__(self):
+        return "success: {}, status code: {}, data: {}".format(
+            self.success, self.status_code, self.data
+        )
+
+
+class Client(object):
+    def __init__(self):
+        self.HOSTNAME = settings.OMDB_HOST_NAME
+        self.API_KEY = settings.OMDB_API_KEY
+
+    def do_get(self, params):
+        url = self.HOSTNAME
+        params["apikey"] = self.API_KEY
+        return requests.get(url, params=params)
+
+    def search(self, search_term):
+        params = {"s": search_term}
+        response = self.do_get(params)
+        return response
