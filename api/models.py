c699f99a23e348633dcc50af60f5a6ab0de71cf4
Andreas Hallden <p.andreas.hallden@gmail.com>
2020-12-08 11:06:44 +0100

    Add str methods to models

--- api/models.py
+++ api/models.py
@@ -7,7 +7,13 @@ class Movie(models.Model):
     name = models.CharField(max_length=255)
     imdbID = models.CharField(max_length=10)
 
+    def __str__(self):
+        return self.name
+
 
 class Favorites(TimeStampedModel):
     name = models.CharField(max_length=255)
-    movies = models.ManyToManyField(Movie, related_name='favorite_lists')
+    movies = models.ManyToManyField(Movie, blank=True, related_name='favorite_lists')
+
+    def __str__(self):
+        return self.name
