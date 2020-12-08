a20d926c2b41219e512e3724089a4e5156d3914c
Andreas Hallden <p.andreas.hallden@gmail.com>
2020-12-08 11:06:51 +0100

    Add admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Favorites, Movie
 
 # Register your models here.
+
+@admin.register(Favorites)
+class FavoritesAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('name',)
